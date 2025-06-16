803f7314b276a1959a6c2a575cbb76eb3aed63ae
opm-ap12345 <prasadankit1010@gmail.com>
2025-06-16 17:18:29 +0000

    container config done

---
+++ Basics/Variable.js
@@ -0,0 +1,14 @@
+
+const var_name=200;
+//constant variable cant change 
+//var_name=10;
+//dont do this as it cant be changed
+let a="this";
+var b="is";
+c="varaible";
+let d;//currently undefined
+//use semicolon or not no issue
+//dont use var , global scope , scope issues
+
+console.log(var_name);
+console.table([var_name,a,b,c,d]);
